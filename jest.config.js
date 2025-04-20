84ffa7eb6fe3b5fd1b788d14589e38e1c23d9938
Lucas Santana <Lucassantana_m@hotmail.com>
2025-04-20 18:56:11 -0300

    Corrigido erro de referência de FormData no teste

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
   preset: 'jest-expo',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',  // Transforma arquivos JS, JSX, TS, TSX
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-navigation|expo(nent)?|@expo(nent)?/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@react-native-community/.*)',
   ],
   setupFiles: [
-    'formdata-polyfill', // Garantir que o polyfill seja carregado antes dos testes
-  ],
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',  // Se necessário
-  ],
+    'formdata-polyfill', // Adicionando o polyfill aqui
+    '@testing-library/jest-native/extend-expect'
+  ], // Se necessário
 };
--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -3,6 +3,8 @@ import { Login } from '../screens/login/login.tsx';
 import { Alert } from 'react-native';
 import { router } from 'expo-router'; // Importando o router para usar no teste
 
+global.FormData = require('formdata-polyfill');
+
 jest.mock('expo-router', () => ({
   useRouter: () => ({
     replace: jest.fn(),
